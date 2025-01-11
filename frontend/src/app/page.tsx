132164dbec450518bd7ec37e735636d5cbd6ffda
pedro cruz <pedrofcrux@gmail.com>
2025-01-10 22:25:32 -0300

    exemplo de integração

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,25 @@
 
+'use client';
+
 export default function Home() {
+  const callAPI = async () => {
+    try {
+      const res = await fetch(`http://localhost:8000/hello`, {
+        mode:'no-cors',
+        method: "get",
+            headers: {
+                 "Content-Type": "application/json"
+            },
+      })
+      const data = await res.json();
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  
   return (
-    <div className="w-full h-full flex justify-center text-black">Bem vindo ao Portal Galt
-    </div>
+    <div className="w-full h-full flex justify-center text-black">Bem vindo ao Portal
+    <button onClick={callAPI}>clica</button></div>
   );
 }
